ccb0788e66b8b443b666243c164e552068f8140c
Jack O'Connor <jackoconnor21@hotmail.co.uk>
2018-05-03 10:56:45 +0100

    Removing scripts with non-recommended removal methods of appx packages

--- RemoveAppsProvisioned.ps1
+++
@@ -1,9 +0,0 @@
-# A pre-defined list of all application package names to remove
-$appXPackages = @('3DBuilder', 'BingFinance', 'BingSports', 'CommsPhone', 'ConnectivityStore', 'Getstarted', 'Messaging', 'MicrosoftOfficeHub',`
-                    'Sway', 'People', 'windowscommunicationsapps', 'WindowsStore', 'Xbox', 'ZuneMusic', 'ZuneVideo', 'MicrosoftSolitaireCollection',` 
-                    'SkypeApp', 'Maps', 'Phone') 
-
-foreach ($i in $appXPackages) {
-    # Remove the provisioned package if it exists to stop the app being deployed to the OS on profile creation
-    Get-AppxProvisionedPackage -online | Where-Object -Property PackageName -Like -Value ("*"+$i+"*") | Remove-AppxProvisionedPackage -Online
-}
