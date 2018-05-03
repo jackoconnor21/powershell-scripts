85ed695542cc320159fb70b779e68fe797f04d89
Jack O'Connor <jackoconnor21@hotmail.co.uk>
2018-05-03 10:55:30 +0100

    PS script to instantly invoke baseline evals on machines
    For use with SCCM 1802 'run script' feature. (can be ran on individual machines also)

---
+++ Invoke-BaselineEvaluation.ps1
@@ -0,0 +1,13 @@
+﻿#####################################################################################
+#
+#    Author: Jack O'Connor
+#    Description: Invoke an evaluation of baselines deployed to selected machines
+#    Website: https://github.com/JackOconnor21
+#    
+#    Using SCCM 1802 'run script' - run this script on a device collection to
+#    invoke an evaluation of each baseline on the machines within the collection.
+#
+#####################################################################################
+
+$ConfigBaselines = Get-WmiObject -ComputerName $env:COMPUTERNAME -Namespace root\ccm\dcm -Class SMS_DesiredConfiguration
+$ConfigBaselines | % { ([wmiclass]"\\$env:COMPUTERNAME\root\ccm\dcm:SMS_DesiredConfiguration").TriggerEvaluation($_.Name, $_.Version) }
