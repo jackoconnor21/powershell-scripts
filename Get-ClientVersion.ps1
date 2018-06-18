7714b1cf9e0d254f67de128fe299172be81aa526
Jack O'Connor <jackoconnor21@hotmail.co.uk>
2018-06-18 12:50:11 +0100

    Adding documentation script and client version script

---
+++ Get-ClientVersion.ps1
@@ -0,0 +1,22 @@
+﻿#########################################################################
+#
+#  Author: Jack O'Connor
+#  Website: https://github.com/JackOconnor21
+#  Modified By: Your name here...
+#  Description: Simple script to use on a local machine or via SCCMs
+#     'run script' feature to find the client version of a machine
+#     in the event SCCM has not yet updated the machine records.
+#  
+#  Edit the $CurrClientVer variable to reflect the latest client version
+#  which is what we will test against.
+#
+#########################################################################
+
+$CurrClientVer = "5.00.8577.1115"
+$MyClientVer = (Get-WMIObject -Namespace root\ccm -Class SMS_Client).ClientVersion
+
+If ($MyClientVer -eq $CurrClientVer) {
+    Write-Host "Up to date" -ForegroundColor Green
+} Else {
+    Write-Host "Out of date ($MyClientVer)" -ForegroundColor Red
+}
