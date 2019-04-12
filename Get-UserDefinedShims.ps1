890b257142aa9c085028f18a5edf23c3ec818007
Jack O'Connor <jackoconnor21@hotmail.co.uk>
2019-04-12 15:57:17 +0100

    PS Script to document user defined shims from the registry

---
+++ Get-UserDefinedShims.ps1
@@ -0,0 +1,23 @@
+﻿######################################################
+#
+#     Author: Jack O'Connor
+#     Description: 
+#
+######################################################
+
+param (
+    [string]$EXEName = "StockViewer.exe" # The executble your SDB is based on
+)
+
+$AppCompatReg = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags"
+
+Get-Item -Path "$($AppCompatReg)\Custom\$EXEName"
+
+$SDBName = (Get-ItemProperty -Pat "$($AppCompatReg)\Custom\$ExeName" | Select-Object -Property "*.sdb").PsObject.Properties.Name
+
+$SDB = Get-Item -Path ("$($AppCompatReg)\InstalledSDB\$SDBName").Replace(".sdb", "")
+
+($SDB | Get-ItemProperty).DatabasePath
+($SDB | Get-ItemProperty).DatabaseType
+($SDB | Get-ItemProperty).DatabaseDescription
+($SDB | Get-ItemProperty).DatabaseInstallTimeStamp
